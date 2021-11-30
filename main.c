5536e9ae365602c7c19f20e33e1781bef810d49a
email <krista5055@yahoo.com>
2021-11-29 17:58:20 -0800

    second commit

--- main.c
+++ main.c
@@ -63,3 +63,56 @@ void lsh_loop(void)
             }
          }
       }
+
+      char *lsh_read_line(void)
+      {
+          char *line = NULL;
+          ssize_t bufsize = 0; // have getline allocate a buffer for us
+
+          if (getline(&line, &bufsize, stdin) == -1){
+              if(feof9stdin)) {
+                  exit(EXIT_SUCCESS); // We received an EOF
+              } else {
+                  perror("readline");
+                  exit)EXIT_FAILURE);printf
+              }
+              }
+
+              return line;
+          }
+
+          #define LSH_TOK_BUFSIZE 64
+          #define LSH_TOK_DELIM " \t\r\n\a"
+          char **lsh_split_line(char *line)
+          {
+              int bufsize = LSH_TOK_BUFSIZE, position = 0;
+              char **tokens = malloc(bufsize * sizeof(char*));
+              char *token;
+
+              if (!tokens){
+                  fprintf(stderr, "lsh: allocation error\n");
+                  exit(EXIT_FAILURE);
+              }
+
+              token = strtok(line, LSH_TOK_DELIM);
+              while (token != NULL) {
+                  tokens[position] = token;
+                  position++;
+
+                  if (position >= bufsize){
+                      bufsize += LSH_TOK_BUFSIZE;
+                      tokens = realloc(tokens, bufsize * sizeof(char*));
+                      if (!tokens) {
+                          fprintf(stderr, "lsh: allocation error\n");
+                          exit(EXIT_FAILURE);
+                      }
+                  }
+
+                  token = strtok(NULL, LSH_TOK_DELIM);
+              }
+              tokens[position] = NULL;
+              return tokens;
+              }
+          }
+      
+
