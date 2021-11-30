728a0be1effb6d240b2f5b6fc80d3edbd13379d7
email <krista5055@yahoo.com>
2021-11-29 18:22:54 -0800

    third commit

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 int main(int argc, char **argv)
 {
     // Load config files, if any.
@@ -70,11 +76,11 @@ void lsh_loop(void)
           ssize_t bufsize = 0; // have getline allocate a buffer for us
 
           if (getline(&line, &bufsize, stdin) == -1){
-              if(feof9stdin)) {
+              if(feof(stdin)) {
                   exit(EXIT_SUCCESS); // We received an EOF
               } else {
                   perror("readline");
-                  exit)EXIT_FAILURE);printf
+                  exit(EXIT_FAILURE);
               }
               }
 
@@ -113,6 +119,95 @@ void lsh_loop(void)
               tokens[position] = NULL;
               return tokens;
               }
-          }
+
+              int lsh_launch(char **args)
+              {
+                  pid_t pid, wpid;
+                  int status;
+
+                  pid = fork();
+                  if (pid == 0){
+                      // Child process
+                      if (execvp(args[0], args) == -1){
+                          perror("lsh");
+                      }
+                      exit(EXIT_FAILURE);
+                  } else if (pid < 0) {
+                      //Error forking
+                      perror("lsh");
+                  } else {
+                      // Parent process
+                      do {
+                          wpid = waitpid(pid, &status, WUNTRACED);
+                      }while (!WIFEXITED(status) && !WIFSIGNALED(status));
+                      }
+
+                      return 1;
+                  }
+                  
+                  int lsh_cd(char **args);
+                  int lsh_help(char **args);
+                  int lsh_exit(char **args);
+
+                  char *builtin_str[] = {
+                      "cd",
+                      "help",
+                      "exit",
+                  };
+
+                  int (*builtin_func[]) (char **) = {
+                      &lsh_cd,
+                      &lsh_help,
+                      &lsh_exit
+                  };
+
+                  int(*builtin_func[]) (char **) = {
+                      &lsh_cd,
+                      &lsh_help,
+                      &lsh_exit
+                  };
+
+                  int lsh_num_builtins() {
+                      return sizeof(builtin_str) / sizeof(char *);
+                  }
+
+                  int lsh_help(char **args)
+                  {
+                      int i;
+                      printf("Stephen Brennan's LSH\n");
+                      printf("{Type program names and arguments, and hit enter.\n");
+                      printf("The following are built in:\n");
+
+                      for (i = 0; i < lsh_num_builtins(); i++) {
+                          printf(" %s\n", builtin_str[i]);
+                      }
+
+                      printf("Use the man command for information on other programs.\n");
+                      return 1;
+                  }
+
+                  int lsh_exit(char **args)
+                  {
+                      return 0;
+                  }
+
+                  int lsh_execute(char **args)
+                  {
+                      int i;
+
+                      if (args[0] == NULL) {
+                          // An empty command was entered.
+                          return 1;
+                      }
+
+                      for (i = 0; i < lsh_num_builtins(); i++){
+                          if (strcmp(args[0], builtin_str[i]) == 0){
+                              return (*builtin_func[i])(args);
+                          }
+                  }
+
+                      return lsh_launch(args);
+                  }
+          
       
 
